d29cc61f46e5148501343356f947f887f962ed36
DS <dhanasekars@gmail.com>
2022-02-25 15:09:48 +0530

    renamed files

---
+++ 0level.py
@@ -0,0 +1,7 @@
+# This is a first challenge with an image and a hit to change the URL
+
+
+print(2**38)
+print(pow(2,38))
+
+#274877906944
--- 2level.py
+++ 2level.py
@@ -1,33 +1,8 @@
-# This is level two with a text to decipher and a hint. 
-# The hint and the cypher looks shift two ASCII value.
-
-import string
-from cs50 import get_string
-
-#string = get_string("input string: ")
-txt = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
-
-new_string = ""
-# for i in txt:
-#     if i.isalpha() == True:
-#         i = (ord(i)+2)  
-#         if i > 122:
-#             i = i - 26
-#             new_string += chr(i) 
-#         else:
-#             new_string += chr(i)
-#     else:
-#         new_string += i
-
-# print(new_string)
-
-
-
-# Better options 
-
+f = open("level3.txt", "r")
+txt = f.read()
+newtxt = ""
 for i in txt:
     if ord(i) >= ord('a') and ord(i) <= ord('z'):
-        new_string += chr((ord(i) + 2 - ord('a')) % 26 + ord('a'))
-    else:
-        new_string += i
-print(new_string)
+        newtxt += i
+print(newtxt)
+
